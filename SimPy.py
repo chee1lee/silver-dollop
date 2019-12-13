83f253d6b4f033a3a141663b40ba34fdf6dc83a3
chee1lee <rtbsha11@gmail.com>
2019-12-13 11:27:04 -0400

    Update SimPy.py
    Add gantt chart for monitoring
    
    "dash" and "numpy" should be installed.
    pip install dash==1.7.0
    pip install numpy
    
    TODO: pycharm doesn't work for "fig.show()"

--- SimPy.py
+++ SimPy.py
@@ -1,6 +1,15 @@
 import simpy
 import random
 import threading
+from datetime import datetime, date, time, timedelta
+
+# for import plotly, Dash should be installed.
+#    pip install dash==1.7.0
+#    pip install numpy
+import plotly.figure_factory as ff
+
+
+FIRST_DATE = date(2010, 1, 1)
 
 
 class GenericArm:
@@ -8,8 +17,10 @@ class GenericArm:
     _capacity = 1
     _key_lock = threading.Lock()
 
-    def __init__(self, capacity):
+    def __init__(self, env, capacity):
+        self._env = env
         self._capacity = capacity
+        self.monitoring_data = []
 
     def put(self, item):
         ret = False
@@ -19,6 +30,9 @@ class GenericArm:
                 ret = True
             else:
                 ret = False
+
+        self.monitoring_data.append((self._env.now, item.value['id']))
+
         return ret
 
     def get(self, state):
@@ -34,6 +48,9 @@ class GenericArm:
                         ret = buffer
                     except ValueError:
                         print("Error to remove")
+
+        self.monitoring_data.append((self._env.now, ret.value['id']))
+
         return ret
 
     def clear(self):
@@ -50,7 +67,7 @@ def wafer_producer(env, robotram, wafers, events):
         print(env.now, 'Produced wafer', wafers[i])
         yield env.timeout(2)
         events['produced'].succeed(value='produced')
-        #events['produced'] = env.event()  # update triggered old event as new one.
+        # events['produced'] = env.event()  # update triggered old event as new one.
         yield env.timeout(15)
 
 
@@ -109,7 +126,7 @@ def handler(env, entry, robotArm, chamber_1st, chamber_2nd, events):
             yield env.timeout(2)
             if not events['handler'].triggered:
                 events['handler'].succeed(value='handler')
-            #break
+            # break
 
         elif fromwho == 'handler':
             events['handler'] = env.event()  # update triggered old event as new one.
@@ -135,7 +152,7 @@ def handler(env, entry, robotArm, chamber_1st, chamber_2nd, events):
                     events['chamber1 requested'].succeed(value='chamber1 requested')
 
         elif fromwho == 'produced':
-            #print(env.now, 'from producer')
+            # print(env.now, 'from producer')
             events['produced'] = env.event()  # update triggered old event as new one.
             if robotArm.count() == 0:
                 wafer = entry.get()
@@ -155,13 +172,30 @@ class MonitoredStore(simpy.Store):
         self.data = []
 
     def put(self, *args, **kwargs):
-        self.data.append((self._env.now, 'put'))
+        self.data.append((self._env.now, args[0].value['id']))
         return super().put(*args, **kwargs)
 
     def get(self, *args, **kwargs):
-        self.data.append((self._env.now, 'get'))
-        return super().get(*args, **kwargs)
+        ret = super().get(*args, **kwargs)
+        self.data.append((self._env.now, ret.value.value['id']))
+        return ret
 
+
+def convert_to_gantt_data(wafer_log_list, task_id, resource_prefix):
+    ret = []
+    for i in range(1, len(wafer_log_list), 2):
+        ret.append(dict(Task=task_id, Start=FIRST_DATE + timedelta(days=wafer_log_list[i - 1][0]),
+                        Finish=FIRST_DATE + timedelta(days=wafer_log_list[i][0]),
+                        Resource=resource_prefix + str(wafer_log_list[i][1])))
+    return ret
+
+
+def generate_colors(prefix, n):
+    ret = {}
+    for i in range(n):
+        ret[prefix + str(i)] = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
+
+    return ret
 
 
 TOT_WAFERS = 20
@@ -169,17 +203,30 @@ env = simpy.Environment()
 wafers = list()
 for i in range(TOT_WAFERS):
     wafers.append({'id': i, 'state': 'raw', 'time_ch1': random.randint(3, 10), 'time_ch2': random.randint(2, 6)})
-eventsname = ['produced', 'consumed', 'chamber1 finished', 'chamber1 requested', 'chamber2 finished',
-              'chamber2 requested', 'handler']
-events = {event: env.event() for event in eventsname}
+events_names = ['produced', 'consumed', 'chamber1 finished', 'chamber1 requested', 'chamber2 finished',
+                'chamber2 requested', 'handler']
+events = {event: env.event() for event in events_names}
 entry = simpy.Store(env, capacity=1)
-RobotArm = GenericArm(capacity=2)
+RobotArm = GenericArm(env, capacity=2)
 Chamber_1st = MonitoredStore(env, capacity=2)
 Chamber_2nd = MonitoredStore(env, capacity=2)
 
 prod = env.process(wafer_producer(env, entry, wafers, events))
 robotHandler = env.process(handler(env, entry, RobotArm, Chamber_1st, Chamber_2nd, events))
-env.run(until=524)
+env.run(until=515)
 
 print(Chamber_1st.data)
 print(Chamber_2nd.data)
+print(RobotArm.monitoring_data)
+
+data_gantt_ch1 = convert_to_gantt_data(Chamber_1st.data, 'Chamber1', 'Wafer')
+data_gantt_ch2 = convert_to_gantt_data(Chamber_2nd.data, 'Chamber2', 'Wafer')
+data_gantt_robot = convert_to_gantt_data(RobotArm.monitoring_data, 'Robot')
+
+gen_colors = generate_colors('Wafer', 100)
+
+fig = ff.create_gantt(data_gantt_ch1 + data_gantt_ch2 + data_gantt_robot, colors=gen_colors, index_col='Resource', show_colorbar=True, showgrid_x=True, showgrid_y=True, group_tasks=True)
+
+# following doesn't work in pycharm.
+# work in jupyter notebook
+# fig.show()
